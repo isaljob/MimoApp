fbe96eb1009378be59347643e5c65deca1af3d83
Faishal Fawwaz <isaljob@gmail.com>
2019-11-28 05:49:51 +0700

    siap-siap deploy ke github

---
+++ js/main.js
@@ -0,0 +1,187 @@
+// init sidenav ketika content berhasil di load 
+document.addEventListener('DOMContentLoaded', function (event) {
+    var sideNav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sideNav);
+
+    // menangkap huruf kemudian play bunyi
+    const a = document.querySelector('.a');
+    a.addEventListener('click', function () {
+        const bunyi = a.children[1];
+        bunyi.play();
+    });
+
+    const ba = document.querySelector('.ba');
+    ba.addEventListener('click', function () {
+        const bunyi = ba.children[1];
+        bunyi.play();
+    });
+
+    const ta = document.querySelector('.ta');
+    ta.addEventListener('click', function () {
+        const bunyi = ta.children[1];
+        bunyi.play();
+    });
+
+    const tsa = document.querySelector('.tsa');
+    tsa.addEventListener('click', function () {
+        const bunyi = tsa.children[1];
+        bunyi.play();
+    });
+
+    const ja = document.querySelector('.ja');
+    ja.addEventListener('click', function () {
+        const bunyi = ja.children[1];
+        bunyi.play();
+    });
+
+    const ha = document.querySelector('.ha');
+    ha.addEventListener('click', function () {
+        const bunyi = ha.children[1];
+        bunyi.play();
+    });
+
+    const kho = document.querySelector('.kho');
+    kho.addEventListener('click', function () {
+        const bunyi = kho.children[1];
+        bunyi.play();
+    });
+
+    const da = document.querySelector('.da');
+    da.addEventListener('click', function () {
+        const bunyi = da.children[1];
+        bunyi.play();
+    });
+
+    const dza = document.querySelector('.dza');
+    dza.addEventListener('click', function () {
+        const bunyi = dza.children[1];
+        bunyi.play();
+    });
+
+    const ra = document.querySelector('.ra');
+    ra.addEventListener('click', function () {
+        const bunyi = ra.children[1];
+        bunyi.play();
+    });
+
+    const za = document.querySelector('.za');
+    za.addEventListener('click', function () {
+        const bunyi = za.children[1];
+        bunyi.play();
+    });
+
+    const sa = document.querySelector('.sa');
+    sa.addEventListener('click', function () {
+        const bunyi = sa.children[1];
+        bunyi.play();
+    });
+
+    const sya = document.querySelector('.sya');
+    sya.addEventListener('click', function () {
+        const bunyi = sya.children[1];
+        bunyi.play();
+    });
+
+    const sha = document.querySelector('.sha');
+    sha.addEventListener('click', function () {
+        const bunyi = sha.children[1];
+        bunyi.play();
+    });
+
+    const dha = document.querySelector('.dha');
+    dha.addEventListener('click', function () {
+        const bunyi = dha.children[1];
+        bunyi.play();
+    });
+
+    const tha = document.querySelector('.tha');
+    tha.addEventListener('click', function () {
+        const bunyi = tha.children[1];
+        bunyi.play();
+    });
+
+    const zha = document.querySelector('.zha');
+    zha.addEventListener('click', function () {
+        const bunyi = zha.children[1];
+        bunyi.play();
+    });
+
+    const ain = document.querySelector('.ain');
+    ain.addEventListener('click', function () {
+        const bunyi = ain.children[1];
+        bunyi.play();
+    });
+
+    const ghin = document.querySelector('.ghin');
+    ghin.addEventListener('click', function () {
+        const bunyi = ghin.children[1];
+        bunyi.play();
+    });
+
+    const fa = document.querySelector('.fa');
+    fa.addEventListener('click', function () {
+        const bunyi = fa.children[1];
+        bunyi.play();
+    });
+
+    const qo = document.querySelector('.qo');
+    qo.addEventListener('click', function () {
+        const bunyi = qo.children[1];
+        bunyi.play();
+    });
+
+    const ka = document.querySelector('.ka');
+    ka.addEventListener('click', function () {
+        const bunyi = ka.children[1];
+        bunyi.play();
+    });
+
+    const la = document.querySelector('.la');
+    la.addEventListener('click', function () {
+        const bunyi = la.children[1];
+        bunyi.play();
+    });
+
+    const ma = document.querySelector('.ma');
+    ma.addEventListener('click', function () {
+        const bunyi = ma.children[1];
+        bunyi.play();
+    });
+
+    const na = document.querySelector('.na');
+    na.addEventListener('click', function () {
+        const bunyi = na.children[1];
+        bunyi.play();
+    });
+
+    const wa = document.querySelector('.wa');
+    wa.addEventListener('click', function () {
+        const bunyi = wa.children[1];
+        bunyi.play();
+    });
+
+    const haa = document.querySelector('.haa');
+    haa.addEventListener('click', function () {
+        const bunyi = haa.children[1];
+        bunyi.play();
+    });
+
+    const lamAlif = document.querySelector('.lamAlif');
+    lamAlif.addEventListener('click', function () {
+        const bunyi = lamAlif.children[1];
+        bunyi.play();
+    });
+
+    const hamzah = document.querySelector('.hamzah');
+    hamzah.addEventListener('click', function () {
+        const bunyi = hamzah.children[1];
+        bunyi.play();
+    });
+
+    const ya = document.querySelector('.ya');
+    ya.addEventListener('click', function () {
+        const bunyi = ya.children[1];
+        bunyi.play();
+    });
+
+});
